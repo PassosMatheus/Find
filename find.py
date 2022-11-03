fd706a0b7394b2f007df1c226e85f7d1b5c593e9
Matheus Passos <matheus.martinspassos@yahoo.com>
2022-11-03 14:26:46 -0300

    Criado o inicio do programa, que já consegue encontrar os arquivos especificados

---
+++ find.py
@@ -0,0 +1,36 @@
+import os
+
+caminho_procura = '/Users/EG0265/Desktop/Particular/Igreja/Mocidade'
+termo_procura = 'imagem'
+
+
+
+conta = 0
+for raiz, diretorios, arquivos in os.walk(caminho_procura):
+    for arquivo in arquivos:
+        if termo_procura in arquivo:
+            try:
+                conta += 1
+                caminho_completo = os.path.join(raiz, arquivo)
+                nome_arquivo, ext_arquivo = os.path.splitext(arquivo)
+                tamanho = os.path.getsize(caminho_completo)
+
+                print()
+                print('Encontrei o arquivo:', arquivo)
+                print('Caminho:', caminho_completo)
+                print('Nome:', nome_arquivo)
+                print('Extensão:', ext_arquivo)
+                print('Tamanho:', tamanho)
+            except PermissionError as e:
+                print('Sem permissão.')
+            except FileNotFoundError as e:
+                print('Arquivo não encontrado.')
+            except Exception as e:
+                print('Erro desconhecido:', e)
+
+print()
+print(f'{conta} arquivo(s) encontrado(s).')
+
+
+
+
